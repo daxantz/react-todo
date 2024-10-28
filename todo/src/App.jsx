d71b685514634b792cea00c36ed26dc238a4465c
daxantz <daxantz@gmail.com>
2024-10-28 13:48:07 -0400

    moved components into a main page component

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,24 +1,9 @@
-import Search from "./components/search/Search";
-import Dropdown from "./components/dropdown/Dropdown";
-import MenuItem from "./components/dropdown/MenuItem";
+import MainPage from "./pages/MainPage";
+
 function App() {
-  const options = [
-    { name: "Default" },
-    { name: "Ascending Date" },
-    { name: "Decending Date" },
-    { name: "Ascending Complexity" },
-    { name: "Decending Complexity" },
-    { name: "Ascending Priority" },
-    { name: "Decending Priority" },
-  ];
   return (
     <>
-      <Search />
-      <Dropdown title="filter">
-        {options.map((option) => (
-          <MenuItem key={option.name} option={option} />
-        ))}
-      </Dropdown>
+      <MainPage />
     </>
   );
 }
