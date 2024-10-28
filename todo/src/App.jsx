4b4ca8e3083b4b97f20aa00d8d9eed6c20d9aff5
daxantz <daxantz@gmail.com>
2024-10-28 00:37:26 -0400

    removed default code and added search and dropdown components

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Search from "./components/search/Search";
+import Dropdown from "./components/dropdown/Dropdown";
+import MenuItem from "./components/dropdown/MenuItem";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const options = [
+    { name: "Default" },
+    { name: "Ascending Date" },
+    { name: "Decending Date" },
+    { name: "Ascending Complexity" },
+    { name: "Decending Complexity" },
+    { name: "Ascending Priority" },
+    { name: "Decending Priority" },
+  ];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Search />
+      <Dropdown title="filter">
+        {options.map((option) => (
+          <MenuItem key={option.name} option={option} />
+        ))}
+      </Dropdown>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
