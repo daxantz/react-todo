ff15183bb13c5a9e233987d7a365ea256f18cdeb
daxantz <daxantz@gmail.com>
2024-10-28 20:21:23 -0400

    created page component to display todo details

---
+++ todo/src/pages/TaskDetails.jsx
@@ -0,0 +1,32 @@
+import Pageheader from "../components/pageheader/Pageheader";
+import Todo from "../components/todo/Todo";
+import SubtaskItem from "../components/subtasks/SubtaskItem";
+export default function TaskDetails() {
+  const todo = {
+    title: "First todo",
+    dueDate: "2/24/2024",
+    priority: "8/10",
+    complexity: "10/10",
+    subtasks: [
+      { text: "1. Research the company and its culture", id: 1 },
+      { text: "1. Research the company and its culture", id: 3 },
+      { text: "1. Research the company and its culture", id: 4 },
+      { text: "1. Research the company and its culture", id: 2 },
+    ],
+  };
+
+  return (
+    <>
+      <Pageheader title={"Task Details"} />
+      <Todo todo={todo} />
+      <div className="subtasks">
+        <h2>Checklist for subtasks</h2>
+        {todo.subtasks.map((task) => (
+          <SubtaskItem key={task.id} subtask={task} />
+        ))}
+      </div>
+      <button>Repeat Tasks</button>
+      <button>Delete Tasks</button>
+    </>
+  );
+}
