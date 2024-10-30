98c6ba4e328ba8f3ee59b510440530f577c5d1dc
daxantz <daxantz@gmail.com>
2024-10-29 20:33:37 -0400

    added needed props and setlevel function

--- todo/src/components/circle/Circle.jsx
+++ todo/src/components/circle/Circle.jsx
@@ -1,9 +1,27 @@
 import "../circle/Circle.css";
 
-export default function Circle({ level, color }) {
+export default function Circle({
+  level,
+  color,
+  handleSelect,
+  currentLevel,
+  name,
+  setLevel,
+}) {
   return (
-    <div className="Circle" style={{ backgroundColor: color }}>
-      {level && <span>{level}</span>}
+    <div className={`Circle `} style={{ backgroundColor: color }}>
+      {level && (
+        <span
+          className={`${level === currentLevel ? "Selected" : ""}`}
+          id={level}
+          onClick={(e) => {
+            handleSelect(e);
+            setLevel(name, e.target.id);
+          }}
+        >
+          {level}
+        </span>
+      )}
     </div>
   );
 }
