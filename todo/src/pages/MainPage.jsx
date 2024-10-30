3935eef12c8fc126749f985a76c3fed614131c5c
daxantz <daxantz@gmail.com>
2024-10-30 00:00:38 -0400

    added state to hold the current todo and rendered the task detail component if there is a current todo

--- todo/src/pages/MainPage.jsx
+++ todo/src/pages/MainPage.jsx
@@ -2,7 +2,16 @@ import Search from "../components/search/Search";
 import Dropdown from "../components/dropdown/Dropdown";
 import MenuItem from "../components/dropdown/MenuItem";
 import Todo from "../components/todo/Todo";
-export default function MainPage({ todos }) {
+import TaskDetails from "./TaskDetails";
+import { useState } from "react";
+export default function MainPage({ todos, createTodo }) {
+  const [currentTodo, setCurrentTodo] = useState(null);
+
+  function handleClick(id) {
+    const currentTodo = todos.find((todo) => todo.id === id);
+    setCurrentTodo(currentTodo);
+    console.log("the current todo is : ", currentTodo);
+  }
   const sortOptions = [
     { name: "Default" },
     { name: "Ascending Date" },
@@ -16,25 +25,31 @@ export default function MainPage({ todos }) {
   const filterOptions = [{ name: "tag1" }, { name: "tag2" }];
   return (
     <>
-      <Search />
-      <div className="filters">
-        <Dropdown title={"Sort"}>
-          {sortOptions.map((option) => (
-            <MenuItem key={option.name} option={option} type={"radio"} />
-          ))}
-        </Dropdown>
-        <Dropdown title={"Filter"}>
-          {filterOptions.map((option) => (
-            <MenuItem key={option.name} option={option} type={"checkbox"} />
-          ))}
-        </Dropdown>
-      </div>
-      <div className="todos">
-        {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} />
-        ))}
-      </div>
-      <button>+ Add New Task</button>
+      {currentTodo ? (
+        <TaskDetails todo={currentTodo} />
+      ) : (
+        <>
+          <Search />
+          <div className="filters">
+            <Dropdown title={"Sort"}>
+              {sortOptions.map((option) => (
+                <MenuItem key={option.name} option={option} type={"radio"} />
+              ))}
+            </Dropdown>
+            <Dropdown title={"Filter"}>
+              {filterOptions.map((option) => (
+                <MenuItem key={option.name} option={option} type={"checkbox"} />
+              ))}
+            </Dropdown>
+          </div>
+          <div className="todos">
+            {todos.map((todo) => (
+              <Todo key={todo.id} todo={todo} handleClick={handleClick} />
+            ))}
+          </div>
+          <button onClick={createTodo}>+ Add New Task</button>
+        </>
+      )}
     </>
   );
 }
