43285452119f96345b38ab886ef7eeb57ca59fe5
daxantz <daxantz@gmail.com>
2024-10-29 20:39:03 -0400

    added state for all todos

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,9 +1,18 @@
 import MainPage from "./pages/MainPage";
-
+import TaskDetails from "./pages/TaskDetails";
+import CreateTask from "./pages/CreateTask";
+import Form from "./components/form/Form";
+import { useState } from "react";
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
-      <MainPage />
+      <MainPage todos={todos} />
+      {/* <TaskDetails /> */}
+      <CreateTask>
+        <Form setTodos={setTodos} />
+      </CreateTask>
     </>
   );
 }
