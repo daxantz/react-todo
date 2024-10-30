28d0ee9c274940a498eec7e0bc5b8e15833591f4
daxantz <daxantz@gmail.com>
2024-10-29 20:37:42 -0400

    added state for the current level , created function to set current level and passed props to circle component

--- todo/src/components/levelselector/LevelSelector.jsx
+++ todo/src/components/levelselector/LevelSelector.jsx
@@ -1,12 +1,30 @@
 import Circle from "../circle/Circle";
 import "../levelselector/LevelSelector.css";
-export default function LevelSelector({ text, level = 10 }) {
+import { useState } from "react";
+export default function LevelSelector({ level = 10, setLevel, name }) {
+  const [currentLevel, setCurrentLevel] = useState();
+
+  function handleSelect(e) {
+    setCurrentLevel(parseInt(e.target.id));
+    console.log(e.target.id);
+  }
+
   return (
     <div className="LevelSelector">
-      <p>Select {text} level</p>
+      <p>Select {name} level</p>
       <div className="levels">
         {Array.from({ length: level }).map((level, index) => (
-          <Circle key={index} level={index + 1} color={"#0D99FF"} />
+          <>
+            <Circle
+              key={index}
+              level={index + 1}
+              color={currentLevel === index + 1 ? "#0D99FF" : "#0D99FF1A"}
+              handleSelect={handleSelect}
+              currentLevel={currentLevel}
+              name={name}
+              setLevel={setLevel}
+            />
+          </>
         ))}
       </div>
     </div>
