f670e88e7789e44bef5b4af99f246957013cf435
daxantz <daxantz@gmail.com>
2024-10-29 23:59:00 -0400

    removed placeholder todo object and added prop for a todo and made subtasks optional

--- todo/src/pages/TaskDetails.jsx
+++ todo/src/pages/TaskDetails.jsx
@@ -1,19 +1,19 @@
 import Pageheader from "../components/pageheader/Pageheader";
 import Todo from "../components/todo/Todo";
 import SubtaskItem from "../components/subtasks/SubtaskItem";
-export default function TaskDetails() {
-  const todo = {
-    title: "First todo",
-    dueDate: "2/24/2024",
-    priority: "8/10",
-    complexity: "10/10",
-    subtasks: [
-      { text: "1. Research the company and its culture", id: 1 },
-      { text: "1. Research the company and its culture", id: 3 },
-      { text: "1. Research the company and its culture", id: 4 },
-      { text: "1. Research the company and its culture", id: 2 },
-    ],
-  };
+export default function TaskDetails({ todo }) {
+  // const todo = {
+  //   title: "First todo",
+  //   dueDate: "2/24/2024",
+  //   priority: "8/10",
+  //   complexity: "10/10",
+  //   subtasks: [
+  //     { text: "1. Research the company and its culture", id: 1 },
+  //     { text: "1. Research the company and its culture", id: 3 },
+  //     { text: "1. Research the company and its culture", id: 4 },
+  //     { text: "1. Research the company and its culture", id: 2 },
+  //   ],
+  // };
 
   return (
     <>
@@ -21,7 +21,7 @@ export default function TaskDetails() {
       <Todo todo={todo} />
       <div className="subtasks">
         <h2>Checklist for subtasks</h2>
-        {todo.subtasks.map((task) => (
+        {todo?.subtasks.map((task) => (
           <SubtaskItem key={task.id} subtask={task} />
         ))}
       </div>
