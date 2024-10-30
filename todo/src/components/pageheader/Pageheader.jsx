4fcae442bce992f8a201b9b93b89e288553f6287
daxantz <daxantz@gmail.com>
2024-10-30 00:07:59 -0400

    added clickhandler as prop

--- todo/src/components/pageheader/Pageheader.jsx
+++ todo/src/components/pageheader/Pageheader.jsx
@@ -1,9 +1,9 @@
 import "../pageheader/Pageheader.css";
 
-export default function Pageheader({ title }) {
+export default function Pageheader({ title, backToMain }) {
   return (
     <header className="Pageheader">
-      <span>⬅️</span>
+      <span onClick={backToMain}>⬅️</span>
       <h1>{title}</h1>
       <span>✏️</span>
     </header>
