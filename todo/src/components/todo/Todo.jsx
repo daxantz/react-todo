45b97513580ee8f6007b13b34ab555fb4c451d55
daxantz <daxantz@gmail.com>
2024-10-30 23:23:52 -0400

    added state to mark todo item as compete and adds the done styles to the component

--- todo/src/components/todo/Todo.jsx
+++ todo/src/components/todo/Todo.jsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import "../todo/Todo.css";
 
 export default function Todo({ todo, handleClick }) {
+  const [isComplete, setIsComplete] = useState(false);
+
+  function markComplete() {
+    setIsComplete((previous) => !previous);
+  }
   return (
-    <div className="Todo" onClick={() => handleClick(todo.id)}>
+    <div className={`Todo ${isComplete && "done"}`}>
       <div className="header">
         <h1>{todo.name}</h1>
         <div className="options">
           <button>Edit</button>
-          <button>Mark Complete</button>
+          <button onClick={markComplete}>Mark Complete</button>
         </div>
       </div>
-      <p>Due Date: {todo.dueDate}</p>
-      <p>Priority: {todo.priority}</p>
-      <p>Complexity: {todo.complexity}</p>
-      <div className="tags">
-        {todo.tags.map((tag) => (
-          <span key={tag}>{tag}</span>
-        ))}
+      <div className="body" onClick={() => handleClick(todo.id)}>
+        <p>Due Date: {todo.dueDate}</p>
+        <p>Priority: {todo.priority}</p>
+        <p>Complexity: {todo.complexity}</p>
+        <div className="tags">
+          {todo.tags.map((tag) => (
+            <span key={tag}>{tag}</span>
+          ))}
+        </div>
       </div>
     </div>
   );
