f10cad85c28f2e132878a44229c5db1e42702cd7
daxantz <daxantz@gmail.com>
2024-11-02 18:47:23 -0400

    add editing form, editing state and created function to switch between editing states

--- todo/src/pages/TaskDetails.jsx
+++ todo/src/pages/TaskDetails.jsx
@@ -1,24 +1,17 @@
 import Pageheader from "../components/pageheader/Pageheader";
 import Todo from "../components/todo/Todo";
 import SubtaskItem from "../components/subtasks/SubtaskItem";
+import { useState } from "react";
+
+import EditForm from "../components/form/EditForm";
 export default function TaskDetails({
   todo,
   handleToggle,
   setTodos,
   resetTodo,
+  todos,
 }) {
-  // const todo = {
-  //   title: "First todo",
-  //   dueDate: "2/24/2024",
-  //   priority: "8/10",
-  //   complexity: "10/10",
-  //   subtasks: [
-  //     { text: "1. Research the company and its culture", id: 1 },
-  //     { text: "1. Research the company and its culture", id: 3 },
-  //     { text: "1. Research the company and its culture", id: 4 },
-  //     { text: "1. Research the company and its culture", id: 2 },
-  //   ],
-  // };
+  const [isEditing, setIsEditing] = useState(false);
 
   function deleteTodo(id) {
     setTodos((prevTodos) => {
@@ -28,18 +21,35 @@ export default function TaskDetails({
     resetTodo();
   }
 
+  function handleSetEditing() {
+    setIsEditing((previous) => !previous);
+    console.log("editing", todo);
+  }
+
   return (
     <>
-      <Pageheader title={"Task Details"} backToMain={handleToggle} />
-      <Todo todo={todo} />
-      <div className="subtasks">
-        <h2>Checklist for subtasks</h2>
-        {todo?.subtasks.map((task) => (
-          <SubtaskItem key={task.id} subtask={task} />
-        ))}
-      </div>
-      <button>Repeat Task</button>
-      <button onClick={() => deleteTodo(todo.id)}>Delete Task</button>
+      {isEditing ? (
+        <EditForm
+          todoid={todo.id}
+          setTodos={setTodos}
+          setIsEditing={setIsEditing}
+          todos={todos}
+        />
+      ) : (
+        <>
+          <Pageheader title={"Task Details"} backToMain={handleToggle} />
+          <Todo todo={todo} handleEditTodo={handleSetEditing} />
+
+          <div className="subtasks">
+            <h2>Checklist for subtasks</h2>
+            {todo?.subtasks.map((task) => (
+              <SubtaskItem key={task.id} subtask={task} />
+            ))}
+          </div>
+          <button>Repeat Task</button>
+          <button onClick={() => deleteTodo(todo.id)}>Delete Task</button>
+        </>
+      )}
     </>
   );
 }
