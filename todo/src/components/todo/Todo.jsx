840701b459a41dd457ec02c13e7ef0ce4c23dc0e
daxantz <daxantz@gmail.com>
2024-11-02 18:45:24 -0400

    change property names to the match the correct todo property name

--- todo/src/components/todo/Todo.jsx
+++ todo/src/components/todo/Todo.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import "../todo/Todo.css";
 
-export default function Todo({ todo, handleClick }) {
+export default function Todo({ todo, handleClick, handleEditTodo }) {
   const [isComplete, setIsComplete] = useState(false);
 
   function markComplete() {
     setIsComplete((previous) => !previous);
   }
+
   return (
     <div className={`Todo ${isComplete && "done"}`}>
       <div className="header">
-        <h1>{todo.name}</h1>
+        <h1>{todo.taskName}</h1>
         <div className="options">
-          <button>Edit</button>
+          <button onClick={handleEditTodo}>Edit</button>
           <button onClick={markComplete}>Mark Complete</button>
         </div>
       </div>
       <div className="body" onClick={() => handleClick(todo.id)}>
         <p>Due Date: {todo.dueDate}</p>
-        <p>Priority: {todo.priority}</p>
-        <p>Complexity: {todo.complexity}</p>
+        <p>Priority: {todo.Priority}</p>
+        <p>Complexity: {todo.Complexity}</p>
         <div className="tags">
           {todo.tags.map((tag) => (
             <span key={tag}>{tag}</span>
