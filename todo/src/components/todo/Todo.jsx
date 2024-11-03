8e2fcf0bccc058044b03ef0e3f0faee8913b51ab
daxantz <daxantz@gmail.com>
2024-11-02 20:05:47 -0400

    Enhance Todo component to use useEffect for syncing with todos array
    - Added useEffect to update currentTodo when todos prop changes.
    - Introduced currentTodo state to manage the displayed todo data.
    - Improved handling of todo updates within the component lifecycle.

--- todo/src/components/todo/Todo.jsx
+++ todo/src/components/todo/Todo.jsx
@@ -1,28 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../todo/Todo.css";
 
-export default function Todo({ todo, handleClick, handleEditTodo }) {
+export default function Todo({ todo, handleClick, handleEditTodo, todos }) {
   const [isComplete, setIsComplete] = useState(false);
-
+  const [currentTodo, setCurrentTodo] = useState(todo);
   function markComplete() {
     setIsComplete((previous) => !previous);
   }
-
+  useEffect(() => {
+    console.log(todos);
+    if (todos) {
+      let currentTodo = todos.find((t) => t.id === todo.id);
+      setCurrentTodo(currentTodo);
+      console.log("yeee", currentTodo);
+    }
+  }, [todos, todo]);
   return (
     <div className={`Todo ${isComplete && "done"}`}>
       <div className="header">
-        <h1>{todo.taskName}</h1>
+        <h1>{currentTodo.taskName}</h1>
         <div className="options">
           <button onClick={handleEditTodo}>Edit</button>
           <button onClick={markComplete}>Mark Complete</button>
         </div>
       </div>
       <div className="body" onClick={() => handleClick(todo.id)}>
-        <p>Due Date: {todo.dueDate}</p>
-        <p>Priority: {todo.Priority}</p>
-        <p>Complexity: {todo.Complexity}</p>
+        <p>Due Date: {currentTodo.dueDate}</p>
+        <p>Priority: {currentTodo.Priority}</p>
+        <p>Complexity: {currentTodo.Complexity}</p>
         <div className="tags">
-          {todo.tags.map((tag) => (
+          {currentTodo.tags.map((tag) => (
             <span key={tag}>{tag}</span>
           ))}
         </div>
