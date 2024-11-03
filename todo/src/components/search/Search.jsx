8c8b73889f6b7656cbac5cd84d6874fff31087eb
daxantz <daxantz@gmail.com>
2024-11-02 23:21:35 -0400

    added required props for the search input to work

--- todo/src/components/search/Search.jsx
+++ todo/src/components/search/Search.jsx
@@ -1,7 +1,22 @@
-export default function Search() {
+import { useEffect } from "react";
+import "../search/Search.css";
+export default function Search({ search, setSearch, todos, setSearchedTodos }) {
+  function handleSearch(e) {
+    setSearch(e.target.value);
+  }
+
+  useEffect(() => {
+    setSearchedTodos(todos.filter((todo) => todo.taskName === search));
+  }, [search, todos, setSearchedTodos]);
+
   return (
     <form action="" className="Search">
-      <input type="text" placeholder="Search..." />
+      <input
+        type="search"
+        placeholder="Search..."
+        onChange={handleSearch}
+        value={search}
+      />
     </form>
   );
 }
