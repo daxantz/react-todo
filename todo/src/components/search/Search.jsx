74b22f180ae891dd4c91c8ffd9adbf80337ff314
daxantz <daxantz@gmail.com>
2024-11-03 19:47:06 -0500

    added icons to component

--- todo/src/components/search/Search.jsx
+++ todo/src/components/search/Search.jsx
@@ -11,12 +11,16 @@ export default function Search({ search, setSearch, todos, setSearchedTodos }) {
 
   return (
     <form action="" className="Search">
-      <input
-        type="search"
-        placeholder="Search..."
-        onChange={handleSearch}
-        value={search}
-      />
+      <div className="input-wrapper">
+        <span className="icon">🔍</span>
+        <input
+          type="search"
+          placeholder="Search..."
+          onChange={handleSearch}
+          value={search}
+        />
+        <span className="icon-2">➡️</span>
+      </div>
     </form>
   );
 }
