671eeadbb57d19e930857daf242d38d508c9d34f
daxantz <daxantz@gmail.com>
2024-11-04 22:56:32 -0500

    added back icon component

--- todo/src/components/pageheader/Pageheader.jsx
+++ todo/src/components/pageheader/Pageheader.jsx
@@ -1,9 +1,11 @@
+import Back from "../icons/Back";
 import "../pageheader/Pageheader.css";
 
 export default function Pageheader({ title, backToMain }) {
   return (
     <header className="Pageheader">
-      <span onClick={backToMain}>⬅️</span>
+      {/* <span onClick={backToMain}>⬅️</span> */}
+      <Back backToMain={backToMain} />
       <h1>{title}</h1>
       <span>✏️</span>
     </header>
