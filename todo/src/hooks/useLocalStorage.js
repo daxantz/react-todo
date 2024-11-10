ef8c154524ccbf5a8f7d6b8f33839fa2d33fd97a
daxantz <daxantz@gmail.com>
2024-11-09 19:27:15 -0500

    refactored hook to use useEffect to store a value in local storage anytime the setter function is used

--- todo/src/hooks/useLocalStorage.js
+++ todo/src/hooks/useLocalStorage.js
@@ -1,20 +1,34 @@
-export default function useLocalStorage(key) {
-  function setItem(value) {
-    try {
-      localStorage.setItem(key, JSON.stringify(value));
-    } catch (error) {
-      console.log(error);
-    }
-  }
+import { useEffect, useState } from "react";
 
-  function getItem() {
+export default function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
     try {
       const item = localStorage.getItem(key);
-      return item ? JSON.parse(item) : undefined;
+      return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       console.log(error);
     }
-  }
+  });
 
-  return { setItem, getItem };
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+  // function setItem(value) {
+  //   try {
+  //     localStorage.setItem(key, JSON.stringify(value));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // function getItem() {
+  //   try {
+  //     const item = localStorage.getItem(key);
+  //     return item ? JSON.parse(item) : undefined;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  return [value, setValue];
 }
