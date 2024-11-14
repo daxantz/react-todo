c97286a879c6ae32367c748dfd7e8f7ef2ec8328
daxantz <daxantz@gmail.com>
2024-11-13 22:50:57 -0500

    removed uneeded props and use context api

--- todo/src/pages/TaskDetails.jsx
+++ todo/src/pages/TaskDetails.jsx
@@ -1,86 +1,63 @@
 import Pageheader from "../components/pageheader/Pageheader";
 import Todo from "../components/todo/Todo";
 import SubtaskItem from "../components/subtasks/SubtaskItem";
-import { useState } from "react";
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-import EditForm from "../components/form/EditForm";
 import Delete from "../components/icons/Delete";
 import Refresh from "../components/icons/Refresh";
-export default function TaskDetails({
-  todo,
-  handleToggle,
-  setTodos,
-  resetTodo,
-  todos,
-}) {
-  const [isEditing, setIsEditing] = useState(false);
-
+import { TodoContext } from "../../../utils";
+export default function TaskDetails() {
+  const navigate = useNavigate();
+  const { todoid } = useParams();
+  const { value, setValue } = useContext(TodoContext);
+  let currentTodo = value.find((todo) => todo.id === Number(todoid));
+  console.log(currentTodo);
   function deleteTodo(id) {
-    setTodos((prevTodos) => {
+    setValue((prevTodos) => {
       const filteredTodos = prevTodos.filter((todo) => todo.id !== id);
       return filteredTodos;
     });
-    resetTodo();
-  }
-
-  function handleSetEditing() {
-    setIsEditing((previous) => !previous);
-    console.log("editing", todo);
+    navigate(-1);
   }
 
   return (
     <>
-      {isEditing ? (
-        <EditForm
-          todoid={todo.id}
-          setTodos={setTodos}
-          setIsEditing={setIsEditing}
-          todos={todos}
-        />
-      ) : (
-        <>
-          <Pageheader title={"Task Details"} backToMain={handleToggle} />
-          <Todo
-            todo={todo}
-            handleEditTodo={handleSetEditing}
-            todos={todos}
-            todoId={todo.id}
-          />
+      <Pageheader title={"Task Details"} backToMain={navigate} />
+      {currentTodo && <Todo todoId={currentTodo.id} />}
 
-          <div className="subtasks">
-            <h2>Checklist for subtasks</h2>
-            {todo?.subtasks.map((task) => (
-              <SubtaskItem key={task.id} subtask={task} />
-            ))}
-          </div>
-          <button
-            className="btn"
-            style={{
-              padding: "18px 137px",
-              backgroundColor: "#0D99FF",
-              color: "white",
-              borderRadius: "90px",
-              border: "none",
-            }}
-          >
-            <Refresh />
-            <span style={{ marginLeft: "10px" }}>Repeat Task</span>
-          </button>
-          <button
-            className="btn"
-            style={{
-              padding: "18px 137px",
-              backgroundColor: "#FFE0DE",
+      <div className="subtasks">
+        <h2>Checklist for subtasks</h2>
+        {currentTodo?.subtasks.map((task) => (
+          <SubtaskItem key={task.id} subtask={task} />
+        ))}
+      </div>
+      <button
+        className="btn"
+        style={{
+          padding: "18px 137px",
+          backgroundColor: "#0D99FF",
+          color: "white",
+          borderRadius: "90px",
+          border: "none",
+        }}
+      >
+        <Refresh />
+        <span style={{ marginLeft: "10px" }}>Repeat Task</span>
+      </button>
+      <button
+        className="btn"
+        style={{
+          padding: "18px 137px",
+          backgroundColor: "#FFE0DE",
 
-              borderRadius: "90px",
-              border: "none",
-            }}
-            onClick={() => deleteTodo(todo.id)}
-          >
-            <Delete /> Delete Task
-          </button>
-        </>
-      )}
+          borderRadius: "90px",
+          border: "none",
+        }}
+        onClick={() => deleteTodo(currentTodo.id)}
+      >
+        <Delete /> Delete Task
+      </button>
     </>
   );
 }
