5a4a8ccb28ec99258569f1e94dd63faac1198e02
daxantz <daxantz@gmail.com>
2024-11-13 22:54:13 -0500

    implement react router and context api

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,15 +1,44 @@
-import useLocalStorage from "./hooks/useLocalStorage";
 import MainPage from "./pages/MainPage";
 import Wrapper from "./components/wrapper/Wrapper";
-
+import NotFoundPage from "./components/router/NotFoundPage.jsx";
+import Form from "./components/form/Form.jsx";
+import TaskDetails from "./pages/TaskDetails.jsx";
+import EditForm from "./components/form/EditForm.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import useLocalStorage from "./hooks/useLocalStorage";
+import { TodoContext } from "../../utils.js";
 import "../src/styles.css";
 function App() {
   const [value, setValue] = useLocalStorage("todos", []);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainPage />,
+    },
+    {
+      path: "/newtodo",
+      element: <Form />,
+    },
+    {
+      path: "/todos/:todoid",
+      element: <TaskDetails />,
+    },
+    {
+      path: "/todos/edit/:todoid",
+      element: <EditForm />,
+    },
+    {
+      path: "*",
+      element: <NotFoundPage />,
+    },
+  ]);
 
   return (
     <>
       <Wrapper>
-        <MainPage todos={value} setTodos={setValue} />
+        <TodoContext.Provider value={{ value, setValue }}>
+          <RouterProvider router={router} />
+        </TodoContext.Provider>
       </Wrapper>
     </>
   );
