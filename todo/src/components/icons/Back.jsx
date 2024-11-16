0aa2c8ce86880c6be58bbb792261f32963a6e093
daxantz <daxantz@gmail.com>
2024-11-16 18:51:49 -0500

    changed backToMain to use the useNavigate hook

--- todo/src/components/icons/Back.jsx
+++ todo/src/components/icons/Back.jsx
@@ -6,7 +6,7 @@ export default function Back({ backToMain }) {
       viewBox="0 0 44 44"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      onClick={backToMain}
+      onClick={() => backToMain(-1)}
     >
       <circle cx="22" cy="22" r="22" fill="white" />
       <path
