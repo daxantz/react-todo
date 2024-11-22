9c6e18a83c0143ef51a4f3fd25e4f1186e159473
daxantz <daxantz@gmail.com>
2024-11-21 22:52:48 -0500

    remove commented out code

--- todo/src/hooks/useLocalStorage.js
+++ todo/src/hooks/useLocalStorage.js
@@ -13,22 +13,6 @@ export default function useLocalStorage(key, initialValue) {
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [value, key]);
-  // function setItem(value) {
-  //   try {
-  //     localStorage.setItem(key, JSON.stringify(value));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
-
-  // function getItem() {
-  //   try {
-  //     const item = localStorage.getItem(key);
-  //     return item ? JSON.parse(item) : undefined;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
 
   return [value, setValue];
 }
