9272d90b4d6d20d041abb49c9de33f013e1bc30f
daxantz <daxantz@gmail.com>
2024-11-21 22:48:03 -0500

    refactor MainPage for simplicity and Context integration

--- todo/src/pages/MainPage.jsx
+++ todo/src/pages/MainPage.jsx
@@ -1,57 +1,28 @@
 import { Link } from "react-router-dom";
 import Search from "../components/search/Search";
-
 import Todo from "../components/todo/Todo";
-
-import { useContext, useState } from "react";
-
-import { setSortArray, TodoContext } from "../../../utils";
+import { useContext } from "react";
 import usePowerMode from "../hooks/usePowerMode";
-import useSearchTodos from "../hooks/useSearchTodos";
 import Filters from "../components/dropdown/Filters";
+import { TodoContext } from "../contexts/todoContext";
 
 export default function MainPage() {
-  const { value } = useContext(TodoContext);
-  const { search, setSearch, searchedTodos } = useSearchTodos();
-  const [selectedValue, setSelectedValue] = useState();
-  const [tempTodos, setTempTodos] = useState([...value]);
+  const { todos, searchTodos } = useContext(TodoContext);
 
-  const [urgentTodo, setPowerMode, clearUrgentTodo] = usePowerMode(value);
-  function handleChange(event) {
-    setSelectedValue(event.target.value);
-
-    setTempTodos(() => {
-      return setSortArray(
-        event.target.value,
-        event.target.id.split(" ")[0],
-        value
-      );
-    });
-    console.log("temp todos", tempTodos);
-  }
-  function getTodosToDisplay() {
-    //if user searchs for a todo or uses the dropdown to filter the appropriate array will be returned , original todos array returned if no use action
-    const hasSearchedTodos = searchedTodos.length > 0;
-    const hasSelectedValue = selectedValue !== undefined;
-
-    if (hasSearchedTodos) return searchedTodos;
-    if (hasSelectedValue) return tempTodos;
-
-    return value;
-  }
+  const [urgentTodo, setPowerMode, clearUrgentTodo] = usePowerMode(todos);
 
   return (
     <>
-      <Search search={search} setSearch={setSearch} />
-      <Filters handleChange={handleChange} />
+      <Search />
+      <Filters />
 
       {urgentTodo ? (
         <Todo todoId={urgentTodo.id} />
       ) : (
         <div className="todos">
-          {getTodosToDisplay().map((todo) => (
-            <Todo key={todo.id} todoId={todo.id} />
-          ))}
+          {searchTodos
+            ? searchTodos.map((todo) => <Todo key={todo.id} todoId={todo.id} />)
+            : todos.map((todo) => <Todo key={todo.id} todoId={todo.id} />)}
         </div>
       )}
 
