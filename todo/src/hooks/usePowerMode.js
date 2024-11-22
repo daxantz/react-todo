1a4fe9e93f9b5907cce003babec157b8dc234da1
daxantz <daxantz@gmail.com>
2024-11-21 22:52:33 -0500

    added high variable to track the most urgent todo to avoid setting the urgentTodo state everytime an urgent todo is found

--- todo/src/hooks/usePowerMode.js
+++ todo/src/hooks/usePowerMode.js
@@ -4,17 +4,18 @@ export default function usePowerMode(todos) {
   const [urgentTodo, setUrgentTodo] = useState();
 
   function setPowerModeTodo() {
+    let high = todos[0];
     todos.forEach((todo, index) => {
       if (index < todos.length - 1) {
-        const nextTodo = todos[index + 1];
+        let nextTodo = todos[index + 1];
 
         if (todo.urgency < nextTodo.urgency) {
           console.log(nextTodo);
-
-          setUrgentTodo(nextTodo); // Update state to the nextTodo with higher urgency
+          high = todo;
         }
       }
     });
+    setUrgentTodo(high); // Update state to the nextTodo with higher urgency
   }
 
   function clearUrgentTodo() {
