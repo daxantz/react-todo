f9f10c723aaa912076b10f5aec6fdf2f06c3af5e
daxantz <daxantz@gmail.com>
2024-11-23 21:05:33 -0500

    added class

--- todo/src/components/icons/Check.jsx
+++ todo/src/components/icons/Check.jsx
@@ -1,7 +1,7 @@
-export default function Check({ markComplete }) {
+export default function Check({ markComplete, isCompleted }) {
   return (
     <svg
-      className="Check"
+      className={`Check ${isCompleted && "completed"}`}
       width="32"
       height="32"
       viewBox="0 0 32 32"
