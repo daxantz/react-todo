998976aa359a1deb4c5b901c83706c4d514a83d6
daxantz <daxantz@gmail.com>
2024-12-01 21:30:29 -0500

    simplified which todos get rendered depending on the length of the arrays

--- todo/src/pages/MainPage.jsx
+++ todo/src/pages/MainPage.jsx
@@ -7,9 +7,17 @@ import Filters from "../components/dropdown/Filters";
 import { TodoContext } from "../contexts/todoContext";
 
 export default function MainPage() {
-  const { todos, searchTodos } = useContext(TodoContext);
+  const { todos, searchTodos, filteredTodos } = useContext(TodoContext);
 
   const [urgentTodo, setPowerMode, clearUrgentTodo] = usePowerMode(todos);
+
+  let todosToRender = todos;
+
+  if (filteredTodos.length > 0) {
+    todosToRender = filteredTodos;
+  } else if (searchTodos.length > 0) {
+    todosToRender = searchTodos;
+  }
 
   return (
     <>
@@ -20,13 +28,9 @@ export default function MainPage() {
         <Todo todoId={urgentTodo.id} page={"main"} />
       ) : (
         <div className="todos">
-          {searchTodos
-            ? searchTodos.map((todo) => (
-                <Todo key={todo.id} todoId={todo.id} page={"main"} />
-              ))
-            : todos.map((todo) => (
-                <Todo key={todo.id} todoId={todo.id} page={"main"} />
-              ))}
+          {todosToRender.map((todo) => (
+            <Todo key={todo.id} todoId={todo.id} page={"main"} />
+          ))}
         </div>
       )}
 
